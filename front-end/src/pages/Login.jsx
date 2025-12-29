e35075a68e2c54bde8bb3a5059cbe19a4ab2fa12
vctr <victoremaanoel@outlook.com>
2025-12-29 20:55:28 -0300

    fix: adicionando responsividade para login page

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -4,20 +4,32 @@ import { Input } from '@/components/ui/input';
 function Login() {
   return (
     <div className="flex justify-center md:py-10">
-      <div className="bg-card flex min-h-110 w-full max-w-4xl justify-between rounded-xl border shadow-sm">
-        <div className="flex basis-[50%] flex-col items-center justify-center space-y-3 rounded-l-xl rounded-r-[100px] bg-green-700 p-4">
-          <h1 className="text-3xl font-semibold">Olá, Bem Vindo!</h1>
+      <div className="bg-card flex min-h-110 w-full max-w-4xl flex-col rounded-xl border shadow-sm md:flex-row">
+        <div className="flex basis-[40%] flex-col items-center justify-center gap-3 rounded-t-xl rounded-b-[60px] bg-green-700 p-4 md:basis-[50%] md:rounded-l-xl md:rounded-r-[100px]">
+          <h1 className="text-2xl font-semibold md:text-3xl">
+            Olá, Bem Vindo!
+          </h1>
           <p className="text-foreground text-sm">Ainda não tem conta?</p>
-          <Button variant="outline">Registrar-se</Button>
+          <Button variant="outline" size="sm">
+            Registrar-se
+          </Button>
         </div>
-        <div className="flex basis-[50%] flex-col justify-center gap-3 pr-8 pl-8">
-          <h1 className="text-center text-2xl font-semibold">Login</h1>
-          <Input placeholder="Email" />
-          <Input placeholder="Senha" type="password" />
-          <p className="text-foreground text-center text-sm">
+        <div className="flex basis-[60%] flex-col p-4 md:basis-[50%] md:justify-center">
+          <div className="mb-6 flex flex-col gap-3">
+            <h1 className="text-center text-xl font-semibold md:text-2xl">
+              Login
+            </h1>
+            <Input placeholder="Email" className="placeholder:text-sm" />
+            <Input
+              placeholder="Senha"
+              type="password"
+              className="placeholder:text-sm"
+            />
+            <Button className="w-full">Login</Button>
+          </div>
+          <p className="text-muted-foreground text-center text-sm">
             Esqueceu a senha?
           </p>
-          <Button className="w-full">Login</Button>
         </div>
       </div>
     </div>
