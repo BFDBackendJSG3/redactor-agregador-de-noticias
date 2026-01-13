f4a955c54c69598f6fe8038681c726e75977a657
camsgomes <camila.g.santos@estudante.ufcg.edu.br>
2026-01-13 10:20:11 -0300

    US6: Ações para editar perfil de usuario com segurança.

--- back-end/src/routes/user.routes.js
+++ back-end/src/routes/user.routes.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const UserController = require('../controllers/user.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
+const permitEditMiddleware = require('../middlewares/permitEditMiddleware');
 
 const router = Router();
 
 router.get('/', authMiddleware, UserController.listar);
 router.get('/:id', UserController.buscar);
 router.post('/', UserController.criar);
-router.put('/:id', UserController.atualizar);
+router.put('/:id', authMiddleware, permitEditMiddleware, UserController.atualizar);
 router.delete('/:id', UserController.deletar);
 
 module.exports = router;
