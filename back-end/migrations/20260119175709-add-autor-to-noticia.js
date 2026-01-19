be4f4e78d76ef58301f6284de62ece8164bc6107
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2026-01-19 15:15:56 -0300

    Fix: adiciona campo autorId na tabela noticias

---
+++ back-end/migrations/20260119175709-add-autor-to-noticia.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('Noticias', 'autorId', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Usuarios',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'RESTRICT',
+    });
+
+    await queryInterface.addIndex('Noticias', ['autorId']);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Noticias', 'autorId');
+  },
+};
+
