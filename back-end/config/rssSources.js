a8ff9a5ee351e115744e7b83c30360124f421371
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2026-01-20 18:21:09 -0300

    Fix: corrige arquivo de fontes

--- back-end/config/rssSources.js
+++ back-end/config/rssSources.js
@@ -1,14 +1,16 @@
-export const rssSources = [
-  {
-    name: 'G1 Paraíba',
-    url: 'https://g1.globo.com/rss/g1/pb',
-  },
-  {
-    name: 'Portal Correio',
-    url: 'https://portalcorreio.com.br/feed/',
-  },
-  {
-    name: 'Jornal da Paraíba',
-    url: 'https://jornaldaparaiba.com.br/feed',
-  },
-];
+module.exports = {
+  rssSources: [
+    {
+      name: 'G1 Paraíba',
+      url: 'https://g1.globo.com/rss/g1/pb',
+    },
+    {
+      name: 'Portal Correio',
+      url: 'https://portalcorreio.com.br/feed/',
+    },
+    {
+      name: 'Jornal da Paraíba',
+      url: 'https://jornaldaparaiba.com.br/feed',
+    },
+  ],
+};
