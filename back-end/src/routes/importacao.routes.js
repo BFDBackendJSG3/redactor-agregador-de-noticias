b913e1045bb699fb31516191b6ef5db12abfee87
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2026-01-20 16:28:04 -0300

    Fix: exclui arquivos desnecessários referentes a importações antigas

--- back-end/src/routes/importacao.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const ImportacaoController = require('../controllers/importacao.controller');
-
-const router = Router();
-
-router.post('/importar-noticia', ImportacaoController.importar);
-
-module.exports = router;
