a5e3b9c22bc87469195e6bc44b8a1b9d99314e57
vctr <victoremaanoel@outlook.com>
2026-01-25 23:21:19 -0300

    fix: animacao imagem da noticia

--- front-end/src/components/news/NewsRow.jsx
+++ front-end/src/components/news/NewsRow.jsx
@@ -2,12 +2,17 @@ import { Link } from 'react-router-dom';
 
 function NewsRow({ noticia }) {
   return (
-    <Link to={`/noticia/${noticia.id}`} className="rounded-md transition">
-      <img
-        src={noticia.imagemUrl}
-        className="w-full rounded-md object-cover md:h-40 xl:h-45"
-      />
+    <Link to={`/noticia/${noticia.id}`} className="group flex flex-col">
+      {/* IMAGE WRAPPER */}
+      <div className="overflow-hidden rounded-md">
+        <img
+          src={noticia.imagemUrl}
+          className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-105 md:h-45 xl:h-45"
+          alt={noticia.titulo}
+        />
+      </div>
 
+      {/* CONTENT */}
       <div className="mt-1 flex flex-col justify-between">
         <div className="flex justify-between">
           <span className="text-[12px] font-medium text-emerald-600">
